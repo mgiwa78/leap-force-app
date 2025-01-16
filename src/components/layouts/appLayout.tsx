0dc1abb09955f89b58f69c7faff81a3701706961
DebayoTomisin <debayotomisin07@gmail.com>
2025-01-16 19:52:01 +0100

    Chore: make app background green

--- src/components/layouts/appLayout.tsx
+++ src/components/layouts/appLayout.tsx
@@ -8,8 +8,8 @@ interface IAppLayout {
 
 const AppLayout = ({ children }: IAppLayout) => {
   return (
-    <main>
-      <div className="flex flex-col min-h-screen max-w-[1550px] mx-auto py-2 px-3 lg:px-3 lg:pt-4 lg:mb-[100px]">
+    <main className="bg-[#FAFBF5]">
+      <div className="flex flex-col min-h-screen  max-w-[1550px] mx-auto py-2 px-3 lg:px-3 lg:pt-4 lg:mb-[100px]">
         <Menu />
         <div className="mt-4 flex-grow">{children}</div>
       </div>
